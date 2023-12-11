9f6c7f2eae334c53f95959281c635e12eca83302
ibrahim kanber <ibrahim.kanber@qconnex.com>
2023-12-11 22:13:37 +0100

    wip card props

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex bg-orange-200 min-h-screen flex-col items-center justify-between p-24">
-      <Card/>
+      <Card image_url="https://placekitten.com/400/200"/>
     </main>
   );
 }
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,13 +1,27 @@
+"use client"
 import Image from "next/image";
-import React from "react";
 
-const Card = () => {
+import React from "react";
+//image url
+// card basligi
+// paragraph
+// buttonText
+// handle button click
+// favcoun
+
+interface ICard{
+    image_url:string
+}
+
+const Card = (props:ICard) => {
+    console.log(props);
+
   return (
     <div className="max-w-md bg-white rounded-md overflow-hidden shadow-md">
       {/* Resim */}
       <div className="relative h-72">
         <Image
-          src="https://placekitten.com/400/200"
+          src={props.image_url}
           alt="Card Image"
           layout="fill"
           objectFit="cover"
@@ -28,7 +42,7 @@ const Card = () => {
 
       {/* Beğen Butonu */}
       <div className="px-4 pb-4 flex justify-between items-center">
-        <button className="bg-blue-00 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:shadow-outline-blue">
+        <button className="bg-blue-700 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:shadow-outline-blue">
           Beğen
         </button>
         <span className="text-blue-700">12 Beğeni</span>
