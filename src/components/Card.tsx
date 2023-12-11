9091d61acd5d897fd5e4bc28b80718289cee67e7
Ibrahim1210 <kinqofsecred22@gmail.com>
2023-12-11 23:59:22 +0300

    edit card style

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,11 +1,11 @@
-import Image from 'next/image';
-import React from 'react';
+import Image from "next/image";
+import React from "react";
 
 const Card = () => {
   return (
     <div className="max-w-md bg-white rounded-md overflow-hidden shadow-md">
       {/* Resim */}
-      <div className="relative h-32">
+      <div className="relative h-72">
         <Image
           src="https://placekitten.com/400/200"
           alt="Card Image"
@@ -31,7 +31,7 @@ const Card = () => {
         <button className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:shadow-outline-blue">
           Beğen
         </button>
-        <span className="text-gray-600">12 Beğeni</span>
+        <span className="text-blue-700">12 Beğeni</span>
       </div>
     </div>
   );
