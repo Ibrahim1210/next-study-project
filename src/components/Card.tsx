fb91fe8a41879dddaaef93a6841019deee44b694
ibrahim kanber <ibrahim.kanber@qconnex.com>
2023-12-11 21:50:23 +0100

    introduce with tailwind

---
+++ src/components/Card.tsx
@@ -0,0 +1,40 @@
+import Image from 'next/image';
+import React from 'react';
+
+const Card = () => {
+  return (
+    <div className="max-w-md bg-white rounded-md overflow-hidden shadow-md">
+      {/* Resim */}
+      <div className="relative h-32">
+        <Image
+          src="https://placekitten.com/400/200"
+          alt="Card Image"
+          layout="fill"
+          objectFit="cover"
+        />
+      </div>
+
+      {/* Başlık */}
+      <div className="p-4">
+        <h2 className="text-xl font-bold">Card Başlığı</h2>
+      </div>
+
+      {/* Açıklama */}
+      <div className="px-4 pb-2">
+        <p className="text-gray-700">
+          Bu bir card örneğidir. Tailwind CSS kullanılarak oluşturulmuştur.
+        </p>
+      </div>
+
+      {/* Beğen Butonu */}
+      <div className="px-4 pb-4 flex justify-between items-center">
+        <button className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:shadow-outline-blue">
+          Beğen
+        </button>
+        <span className="text-gray-600">12 Beğeni</span>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
