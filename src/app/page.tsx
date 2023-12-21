d0df9c4c96a0eaa0e32cee78eed666a319f0b346
ibrahim kanber <ibrahim.kanber@qconnex.com>
2023-12-21 21:47:09 +0100

    clean up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,14 @@
 "use client";
-import Card from "@/components/Card";
 import Image from "next/image";
 
 
 
 export default function Home() {
-  const handleClick = (e) => {
-    e.stopPropagation()
-    console.log("click");
-    return "ibrahim";
-  };
+
   return (
-    <div className="grid grid-col-2  gap-[2.75rem] bg-orange-200 ">
-      <Card
-        image_url="https://placekitten.com/400/200"
-        card_title="cat"
-        paragraph="lorem"
-        handleClick={handleClick}
-        button_text="Fallow"
-        favcount={5}
-      />
-      <Card
-        image_url="https://placekitten.com/400/200"
-        card_title="cat2"
-        paragraph="lorem"
-        handleClick={handleClick}
-        button_text="Fallow"
-        favcount={5}
-      />
+    <div className="w-full h-96 bg-orange-200 ">
+hello gardasss
+
     </div>
   );
 }
